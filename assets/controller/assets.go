01b8dc054bfc000a41375981a4031963307f4e21
BDDAC117 <mdtahjib75@gmail.com>
2024-03-25 23:31:21 +0600

    feat: QRCODE api added

--- assets/controller/assets.go
+++ assets/controller/assets.go
@@ -22,11 +22,12 @@ func NewAssetController(g interface{}, assetSvc asset_service.AssetService) {
 	}
 	grp := g.(*gin.RouterGroup)
 	grp.POST("v1/asset-details/create", ctl.CreateAsset)
-	grp.PUT("v1/asset-details/update/:id", ctl.UpdateAsset)
+	grp.PUT("v1/asset-details/update/:asset_id", ctl.UpdateAsset)
 	grp.GET("v1/list-of-assets", ctl.ListOfAssets)
-	grp.DELETE("v1/asset-details/delete/:id", ctl.DeleteAsset)
-	grp.GET("v1/asset/:club_id", ctl.GetAssetById)
+	grp.DELETE("v1/asset-details/delete/:asset_id", ctl.DeleteAsset)
+	grp.GET("v1/asset/:asset_id", ctl.GetAssetById)
 	grp.POST("v1/asset-details/csv", ctl.ExportCSV)
+	grp.POST("v1/asset-detail/qrcode/:asset_id", ctl.AssetQRCode)
 
 }
 
@@ -96,7 +97,7 @@ func (ctl *Asset) UpdateAsset(c *gin.Context) {
 	if payloadErr := reqBody.Validate(); payloadErr != nil {
 		log.Fatal("failed to validate request body", payloadErr)
 	}
-	assetId := c.Param("id")
+	assetId := c.Param("asset_id")
 	id, err := strconv.Atoi(assetId)
 	if err != nil {
 		log.Println("failed to convert asset id to int")
@@ -133,7 +134,7 @@ func (ctl *Asset) ListOfAssets(c *gin.Context) {
 }
 
 func (ctl *Asset) DeleteAsset(c *gin.Context) {
-	assetId := c.Param("id")
+	assetId := c.Param("asset_id")
 	id, err := strconv.Atoi(assetId)
 	if err != nil {
 		log.Panicln("failed to convert asset id to int")
@@ -156,12 +157,12 @@ func (ctl *Asset) DeleteAsset(c *gin.Context) {
 }
 
 func (ctl *Asset) GetAssetById(c *gin.Context) {
-	clubID := c.Param("club_id")
+	assetID := c.Param("asset_id")
 
-	id, err := strconv.Atoi(clubID)
+	id, err := strconv.Atoi(assetID)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
-			"message": "internal server error",
+			"message": "failed to convert id to int",
 		})
 		return
 	}
@@ -186,7 +187,6 @@ func (ctl *Asset) ExportCSV(c *gin.Context) {
 		})
 		return
 	}
-
 	csvData, fileName, err := utils.GenerateCSVFromAssetDetails(assets)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{
@@ -194,7 +194,6 @@ func (ctl *Asset) ExportCSV(c *gin.Context) {
 		})
 		return
 	}
-
 	// Upload CSV to S3
 	err = utils.UploadToS3(csvData, fileName)
 	if err != nil {
@@ -203,11 +202,56 @@ func (ctl *Asset) ExportCSV(c *gin.Context) {
 		})
 		return
 	}
-
 	// Set response headers for downloading
 	c.Header("Content-Disposition", "attachment; filename="+fileName)
 	c.Header("Content-Type", "text/csv")
-
 	// Download CSV from S3 and return to client
 	c.Data(http.StatusOK, "text/csv", csvData)
+}
+
+func (ctl *Asset) AssetQRCode(c *gin.Context) {
+	assetID := c.Param("asset_id")
+	id, err := strconv.Atoi(assetID)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "failed to convert id to int",
+		})
+		return
+	}
+	resp, err := ctl.svc.GetAssetsByID(id)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "failed to get asset or internal server error",
+		})
+		return
+	}
+	qrCodeData := dto.AssetQRCode{
+		UserName:      resp.UserName,
+		AssetTag:      resp.AssetTag,
+		SerialNumber:  resp.SerialNumber,
+		AssetType:     resp.AssetType,
+		Location:      resp.Location,
+		PurchasedFrom: resp.PurchasedFrom,
+	}
+	qrCodeBytes, err := utils.GenerateQRCode(qrCodeData)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "failed to generate QR code",
+		})
+		return
+	}
+	err = utils.UploadQRcode(qrCodeBytes, "qrcode/"+qrCodeData.AssetTag+".png")
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"message": "failed to upload qrcode in s3",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "qrcode was successfully uploaded to s3",
+	})
+
+	c.JSON(http.StatusOK, gin.H{
+		"qrCode": qrCodeBytes,
+	})
 }
